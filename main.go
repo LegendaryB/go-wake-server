74fc8a8c5b6896e86864f9edc16a8219a5bfbfd4
LegendaryB <6391022+LegendaryB@users.noreply.github.com>
2021-04-27 01:20:07 +0200

    Added command line argument support.

--- main.go
+++ main.go
@@ -1,25 +1,64 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/linde12/gowol"
 )
 
-func sendWakeOnLan(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	mac := params["mac"]
+type Configuration struct {
+	Port             int
+	UseStaticMac     bool
+	MACAddress       string
+	BroadcastAddress string
+}
 
-	if packet, err := gowol.NewMagicPacket(mac); err == nil {
-		packet.Send("255.255.255.255")
+func WakeOnLANHandler(conf Configuration) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		mac := conf.MACAddress
+
+		if !conf.UseStaticMac {
+			params := mux.Vars(r)
+			mac = params["mac"]
+		}
+
+		if packet, err := gowol.NewMagicPacket(mac); err == nil {
+			packet.Send(conf.BroadcastAddress)
+		} else {
+			http.Error(w, "Not a valid MAC address.", http.StatusBadRequest)
+		}
 	}
 }
 
-func main() {
-	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/wake/{mac}", sendWakeOnLan).Methods("GET")
+func printConfiguration() {
+	fmt.Println("Using configuration:")
 
-	log.Fatal(http.ListenAndServe(":80", router))
+	flag.VisitAll(func(f *flag.Flag) {
+		fmt.Println(f.Name + "\t" + f.Value.String())
+	})
+}
+
+func main() {
+	conf := Configuration{}
+
+	flag.IntVar(&conf.Port, "port", 81, "To specify the http listener port.")
+	flag.BoolVar(&conf.UseStaticMac, "use-static-mac", false, "Flag to indicate if a given mac should be used when the http resource is called.")
+	flag.StringVar(&conf.MACAddress, "mac-addr", "", "Static MAC address which is used when the UseStaticMac flag is set to true.")
+	flag.StringVar(&conf.BroadcastAddress, "broadcast-addr", "255.255.255.255", "Address to which the generated MagicPacket will be send.")
+
+	flag.Parse()
+
+	printConfiguration()
+
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/wake/{mac}", WakeOnLANHandler(conf)).Methods("GET")
+
+	addr := ":" + strconv.Itoa(conf.Port)
+
+	log.Fatal(http.ListenAndServe(addr, router))
 }
